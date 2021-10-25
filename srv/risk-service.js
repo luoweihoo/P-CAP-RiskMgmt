d73b11265f00aa43b7692afb948c3b9883f55c7c
Wei Luo <wei.luo@sap.com>
2021-10-25 18:00:30 +0800

    Change impact level

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -8,7 +8,7 @@ module.exports = cds.service.impl(async function() {
     this.after('READ', 'Risks', risksData => {
         const risks = Array.isArray(risksData) ? risksData : [risksData];
         risks.forEach(risk => {
-            if (risk.impact >= 100000) {
+            if (risk.impact >= 80000) {
                 risk.criticality = 1;
             } else {
                 risk.criticality = 2;
